35e8406cb74f9e96df9fab7be6f462d5004baf09
Vijay <vjsah27@gmail.com>
2015-11-24 18:42:58 -0600

    Committed Client1 config

--- Client1/driver.py
+++ Client1/driver.py
@@ -1,4 +1,4 @@
-import thread
+﻿import thread
 import time
 import socket
 import sys
@@ -8,21 +8,27 @@ import time
 from client import client_module
 from server import server_module
 
+if __name__ == "__main__":
+    config = {}
+    execfile("settings.conf", config) 
+    # python 3: exec(open("example.conf").read(), config)
 
-def execute_client( threadName, delay):
+    
+
+def execute_client( threadName, config):
 	#time.sleep(2)
-	client_module(socket)
+	client_module(socket, config)
 
-def execute_server(threadName):
-	server_module(socket)
+def execute_server(threadName, config):
+	server_module(socket, config)
 
 
 # Create two threads as follows
 try:
-    thread.start_new_thread( execute_server, ("Thread-1",) )
-    thread.start_new_thread( execute_client, ("Thread-2",2,) )
+   thread.start_new_thread( execute_server, ("Thread-1", config) )
+   thread.start_new_thread( execute_client, ("Thread-2", config) )
 except:
-	print "Error: unable to start thread"
+   print "Error: unable to start thread"
 
 while 1:
 	pass
